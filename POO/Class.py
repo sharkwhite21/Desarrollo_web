6810c13101cb2e09ec010ce95435f77347d9e8a6
sharkwhite21 <mbaldovino44@gmail.com>
2019-09-13 15:30:11 -0500

    Definiendo metodo constructor

--- POO/Class.py
+++ POO/Class.py
@@ -1,8 +1,22 @@
-class Curso(object):
+'''class Curso(object):
     nombre= "Marlon"
     def Saludar(self,saludo):
         print( saludo +  self.nombre)
 
 Marlon = Curso()
+
+Marlon.Saludar("Hola mi nombre es ")
+'''
+class Usuario(object):
+    """docstring for Curso."""
+
+    def __init__(self, nombre):
+        #Metodo constructor
+        self.nombre = nombre
+
+    def Saludar(self,saludo):
+        print( saludo +  self.nombre)
+
+Marlon = Usario("Marlon")
 
 Marlon.Saludar("Hola mi nombre es ")
